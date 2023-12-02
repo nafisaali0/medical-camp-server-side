341a3102f0c503f251c05cbc60016e7d29269580
nafisaali0 <nafisaali20006@gmail.com>
2023-12-03 02:37:41 +0600

    setup register camp delete api

--- index.js
+++ index.js
@@ -98,6 +98,14 @@ async function run() {
       const result = await registerCampCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/registerCamps/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      // send data to DB
+      const result = await registerCampCollection.deleteOne(query);
+      res.send(result);
+    });
     // registerCamp db end
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
